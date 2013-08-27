662cf2375a7db91d8a9a5587378a9d1b6d5e5e1d
Justin Tadlock <justin@justintadlock.com>
2013-08-27 12:25:03 -0500

    Typo fix: "messsage" should be "message".

--- admin/meta-box-post-content-permissions.php
+++ admin/meta-box-post-content-permissions.php
@@ -82,7 +82,7 @@ function members_content_permissions_meta_box( $object, $box ) {
 	</p>
 
 	<p>
-		<label for="members_access_error"><?php _e( 'Custom error messsage:', 'members' ); ?></label>
+		<label for="members_access_error"><?php _e( 'Custom error message:', 'members' ); ?></label>
 		<textarea id="members_access_error" name="members_access_error" cols="60" rows="3" tabindex="30" style="width: 99%;"><?php echo esc_html( get_post_meta( $object->ID, '_members_access_error', true ) ); ?></textarea>
 		<br />
 		<span class="howto"><?php _e( 'Message shown to users who do no have permission to view the post.', 'members' ); ?></span>
