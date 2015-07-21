b7cd1f71a927cd2d1c4aa5d722bf0f872265c03f
Justin Tadlock <justin@justintadlock.com>
2015-07-21 15:54:24 -0500

    Cleanup of functions-private-site.php.  Introduces the members_is_private_blog() conditional tag.

--- inc/functions-private-site.php
+++ inc/functions-private-site.php
@@ -1,21 +1,26 @@
 <?php
 /**
- * Handles the private site and private feed features of the plugin.  If private site is selected in the plugin settings,
- * the plugin will redirect all non-logged-in users to the login page.  If private feed is selected, all content is blocked
- * from feeds from the site.
+ * Handles the private site and private feed features of the plugin.  If private site is
+ * selected in the plugin settings, the plugin will redirect all non-logged-in users to the
+ * login page.  If private feed is selected, all content is blocked from feeds from the site.
  *
- * @package Members
- * @subpackage Functions
+ * @package    Members
+ * @subpackage Includes
+ * @author     Justin Tadlock <justin@justintadlock.com>
+ * @copyright  Copyright (c) 2009 - 2015, Justin Tadlock
+ * @link       http://themehybrid.com/plugins/members
+ * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
-/* Redirects users to the login page. */
-add_action( 'template_redirect', 'members_please_log_in', 1 );
+# Redirects users to the login page.
+add_action( 'template_redirect', 'members_please_log_in', 0 );
 
-/* Disable content in feeds if the feed should be private. */
+# Disable content in feeds if the feed should be private.
 add_filter( 'the_content_feed', 'members_private_feed', 95 );
 add_filter( 'the_excerpt_rss',  'members_private_feed', 95 );
 add_filter( 'comment_text_rss', 'members_private_feed', 95 );
 
+# Filters for the feed error message.
 add_filter( 'members_feed_error_message', array( $GLOBALS['wp_embed'], 'run_shortcode' ),   5 );
 add_filter( 'members_feed_error_message', array( $GLOBALS['wp_embed'], 'autoembed'     ),   5 );
 add_filter( 'members_feed_error_message',                              'wptexturize',       10 );
@@ -26,22 +31,33 @@ add_filter( 'members_feed_error_message',                              'do_short
 add_filter( 'members_feed_error_message',                              'shortcode_unautop', 35 );
 
 /**
+ * Conditional tag to see if we have a private blog.
+ *
+ * @since  1.0.0
+ * @access public
+ * @return bool
+ */
+function members_is_private_blog() {
+	return members_get_setting( 'private_blog' );
+}
+
+/**
  * Redirects users that are not logged in to the 'wp-login.php' page.
  *
- * @since 0.1.0
- * @uses is_user_logged_in() Checks if the current user is logged in.
- * @uses auth_redirect() Redirects people that are not logged in to the login page.
+ * @since  0.1.0
+ * @access public
+ * @return void
  */
 function members_please_log_in() {
 
-	/* Check if the private blog feature is active. */
-	if ( members_get_setting( 'private_blog' ) ) {
+	// Check if the private blog feature is active.
+	if ( members_is_private_blog() ) {
 
-		/* If using BuddyPress and on the register page, don't do anything. */
+		// If using BuddyPress and on the register page, don't do anything.
 		if ( function_exists( 'bp_is_current_component' ) && bp_is_current_component( 'register' ) )
 			return;
 
-		/* Else, if the user is not logged in, redirect to the login page. */
+		// Else, if the user is not logged in, redirect to the login page.
 		elseif ( !is_user_logged_in() )
 			auth_redirect();
 	}
@@ -50,9 +66,10 @@ function members_please_log_in() {
 /**
  * Blocks feed items if the user has selected the private feed feature.
  *
- * @since 0.2.0
- * @param string $content The post or comment feed content.
- * @return string $content Returns either the content or an error message.
+ * @since  0.2.0
+ * @access public
+ * @param  string  $content
+ * @return string
  */
 function members_private_feed( $content ) {
 
