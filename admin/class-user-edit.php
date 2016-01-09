ce08220dc943d647644bfe025a2ac358cb41e417
Justin Tadlock <justin@justintadlock.com>
2016-01-09 13:42:27 -0600

    Fix incorrect textdomain.  Fixes: https://github.com/justintadlock/members/issues/76

--- admin/class-user-edit.php
+++ admin/class-user-edit.php
@@ -84,7 +84,7 @@ final class Members_Admin_User_Edit {
 
 		wp_nonce_field( 'new_user_roles', 'members_new_user_roles_nonce' ); ?>
 
-		<h3><?php esc_html_e( 'Roles', 'message-board' ); ?></h3>
+		<h3><?php esc_html_e( 'Roles', 'members' ); ?></h3>
 
 		<table class="form-table">
 
