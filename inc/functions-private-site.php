7db33f0fcbb996bd76d0fabb5ecc1f926dce1efe
Justin Tadlock <justin@justintadlock.com>
2016-06-14 18:39:20 -0500

    Whitelist the BuddyPress activation page in private site mode.  Props @turtlepod
    Fixes: https://github.com/justintadlock/members/issues/83

--- inc/functions-private-site.php
+++ inc/functions-private-site.php
@@ -65,7 +65,7 @@ function members_please_log_in() {
 	if ( members_is_private_blog() && ! is_user_logged_in() ) {
 
 		// If using BuddyPress and on the register page, don't do anything.
-		if ( function_exists( 'bp_is_current_component' ) && bp_is_current_component( 'register' ) )
+		if ( function_exists( 'bp_is_current_component' ) && ( bp_is_current_component( 'register' ) || bp_is_current_component( 'activate' ) ) )
 			return;
 
 		// Redirect to the login page.
