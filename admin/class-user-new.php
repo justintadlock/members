d771f1c7247aa4b3c0d6cfb4a0f0d683d0db427d
Justin Tadlock <justintadlock@gmail.com>
2017-09-28 12:24:11 -0500

    Don't show the multi-role selection form field when adding a new user on multisite. There were numerous issues associated with this that could not be addressed at this stage.
    The commit fixes the issue where no role was being set when adding a new user in multisite.
    
    Fixes: https://github.com/justintadlock/members/issues/153

--- admin/class-user-new.php
+++ admin/class-user-new.php
@@ -48,29 +48,17 @@ final class User_New {
 	private function setup_actions() {
 
 		// If multiple roles per user is not enabled, bail.
-		if ( ! members_multiple_user_roles_enabled() )
+		//
+		// @since 2.0.1 Added a check to not run on multisite.
+		// @link https://github.com/justintadlock/members/issues/153
+		if ( ! members_multiple_user_roles_enabled() || is_multisite() )
 			return;
 
 		// Only run our customization on the 'user-edit.php' page in the admin.
 		add_action( 'load-user-new.php', array( $this, 'load' ) );
 
-		if ( is_multisite() ) {
-
-			add_filter( 'signup_user_meta', array( $this, 'mu_signup_user_meta' ) );
-
-			// Note that `add_new_user_to_blog()` is called with a priority of 10.  That
-			// function sets the role.  So, we need to execute after that.
-			add_action( 'wpmu_activate_user', array( $this, 'mu_activate_user' ), 15, 3 );
-
-			// Hook into the notification email.
-			add_filter( 'wpmu_signup_user_notification_email', array( $this, 'mu_user_notify_email' ), 5 );
-
-		} else {
-
-			// Sets the new user's roles.
-			add_action( 'user_register', array( $this, 'user_register' ) );
-		}
-
+		// Sets the new user's roles.
+		add_action( 'user_register', array( $this, 'user_register' ) );
 	}
 
 	/**
@@ -200,120 +188,6 @@ final class User_New {
 					$user->remove_role( $old_role );
 			}
 		}
-	}
-
-	/**
-	 * Called on multisite when a new user is created. We need to store the roles
-	 * as part of the signup metadata.  These will get assigned to the user once
-	 * the user has been activated.
-	 *
-	 * @since  2.0.1
-	 * @access public
-	 * @param  array   $meta
-	 * @return array
-	 */
-	public function mu_signup_user_meta( $meta ) {
-
-		// If the current user can't promote users or edit this particular user, bail.
-		if ( ! current_user_can( 'promote_users' ) )
-			return $meta;
-
-		// Verify the new role nonce?
-		if ( ! isset( $_POST['members_new_user_roles_nonce'] ) || ! wp_verify_nonce( $_POST['members_new_user_roles_nonce'], 'new_user_roles' ) )
-			return $meta;
-
-		// If we have an array of roles.
-		if ( ! empty( $_POST['members_user_roles'] ) ) {
-
-			// Sanitize the posted roles.
-			$new_roles = array_map( 'members_sanitize_role', $_POST['members_user_roles'] );
-
-			$meta['members_user_roles'] = array();
-
-			foreach ( $new_roles as $new_role ) {
-
-				if ( members_is_role_editable( $new_role ) )
-					$meta['members_user_roles'][] = $new_role;
-			}
-
-			// Makes sure that the `new_role` meta gets passed through.  WP needs this
-			// when first setting up the user signup.  We're going to fix roles later.
-			if ( empty( $_REQUEST['role'] ) || ! $meta['new_role'] && isset( $meta['members_user_roles'][0] ) ) {
-
-				$meta['new_role'] = $meta['members_user_roles'][0];
-			}
-
-		// If no roles were chosen and no new role is set, give the new user the default role.
-		} elseif ( empty( $meta['new_role'] ) ) {
-
-			$meta['new_role'] = get_option( 'default_role' );
-		}
-
-		return $meta;
-	}
-
-	/**
-	 * Callback function when a new user is activated in multisite.  This function
-	 * gets the roles we stored as meta upon signup and assigns them to the user.
-	 *
-	 * @since  2.0.1
-	 * @access public
-	 * @param  int     $user_id
-	 * @param  string  $password
-	 * @param  array   $meta
-	 * @return void
-	 */
-	public function mu_activate_user( $user_id, $password, $meta ) {
-
-		// If we have an array of roles.
-		if ( ! empty( $meta['members_user_roles'] ) ) {
-
-			// Create a new user object.
-			$user = new \WP_User( $user_id );
-
-			// Get the current user roles.
-			$old_roles = (array) $user->roles;
-
-			// Sanitize the posted roles.
-			$new_roles = array_map( 'members_sanitize_role', $meta['members_user_roles'] );
-
-			// Loop through the posted roles.
-			foreach ( $new_roles as $new_role ) {
-
-				// If the user doesn't already have the role, add it.
-				if ( ! in_array( $new_role, (array) $user->roles ) )
-					$user->add_role( $new_role );
-			}
-
-			// Loop through the current user roles.
-			foreach ( $old_roles as $old_role ) {
-
-				// If the role is not in the new roles array, remove it.
-				if ( ! in_array( $old_role, $new_roles ) )
-					$user->remove_role( $old_role );
-			}
-		}
-	}
-
-	/**
-	 * The core WP function `admin_created_user_email()` relies on a hardcoded
-	 * `$_REQUEST['role']`.  So, we're hijacking the email and sending our own.
-	 *
-	 * @since  2.0.1
-	 * @access public
-	 * @param  string  $text
-	 * @return string
-	 */
-	public function ms_user_notify_email( $text ) {
-
-		remove_filter( 'wpmu_signup_user_notification_email', 'admin_created_user_email' );
-
-		return sprintf(
-			// Translators: 1 is the site name, 2 is the site URL, and %%s is replaced with the activate URL.
-			esc_html__( "You've been invited to join %1$s at %2$s. Please click the following link to activate your user account: %%s", 'members' ),
-			esc_html( get_bloginfo( 'name' ) ),
-			esc_url( home_url() )
-		);
 	}
 
 	/**
