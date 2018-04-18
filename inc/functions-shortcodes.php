e1d5b1cdbb415276fd8dcf459c74eaecc3f0bc81
Justin Tadlock <justintadlock@gmail.com>
2018-04-18 12:18:41 -0500

    Adds the `user_name`, `user_id`, and `user_email` parameters to the `[members_access]` shortcode. These work the same as the `role` and `capability` parameters. Multiple values can passed to a given parameter by separating the values with a comma.

--- inc/functions-shortcodes.php
+++ inc/functions-shortcodes.php
@@ -114,6 +114,9 @@ function members_access_check_shortcode( $attr, $content = null ) {
 	$defaults = array(
 		'capability' => '',  // Single capability or comma-separated multiple capabilities.
 		'role'       => '',  // Single role or comma-separated multiple roles.
+		'user_id'    => '',  // Single user ID or comma-separated multiple IDs.
+		'user_name'  => '',  // Single user name or comma-separated multiple names.
+		'user_email' => '',  // Single user email or comma-separated multiple emails.
 		'operator'   => 'or' // Only the `!` operator is supported for now.  Everything else falls back to `or`.
 	);
 
@@ -145,6 +148,56 @@ function members_access_check_shortcode( $attr, $content = null ) {
 			return members_current_user_has_role( $roles ) ? '' : do_shortcode( $content );
 
 		return members_current_user_has_role( $roles ) ? do_shortcode( $content ) : '';
+	}
+
+	$user_id = 0;
+	$user_name = $user_email = '';
+
+	if ( is_user_logged_in() ) {
+
+		$user       = wp_get_current_user();
+		$user_id    = get_current_user_id();
+		$user_name  = $user->user_login;
+		$user_email = $user->user_email;
+	}
+
+	// If the current user has one of the user ids.
+	if ( $attr['user_id'] ) {
+
+		// Get the user IDs.
+		$ids = array_map( 'trim', explode( ',', $attr['user_id'] ) );
+
+		if ( '!' === $operator ) {
+			return in_array( $user_id, $ids ) ? '' : do_shortcode( $content );
+		}
+
+		return in_array( $user_id, $ids ) ? do_shortcode( $content ) : '';
+	}
+
+	// If the current user has one of the user names.
+	if ( $attr['user_name'] ) {
+
+		// Get the user names.
+		$names = array_map( 'trim', explode( ',', $attr['user_name'] ) );
+
+		if ( '!' === $operator ) {
+			return in_array( $user_name, $names ) ? '' : do_shortcode( $content );
+		}
+
+		return in_array( $user_name, $names ) ? do_shortcode( $content ) : '';
+	}
+
+	// If the current user has one of the user emails.
+	if ( $attr['user_email'] ) {
+
+		// Get the user emails.
+		$emails = array_map( 'trim', explode( ',', $attr['user_email'] ) );
+
+		if ( '!' === $operator ) {
+			return in_array( $user_email, $emails ) ? '' : do_shortcode( $content );
+		}
+
+		return in_array( $user_email, $emails ) ? do_shortcode( $content ) : '';
 	}
 
 	// Return an empty string if we've made it to this point.
