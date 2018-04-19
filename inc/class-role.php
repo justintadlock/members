259f6b56946f555ee6a0176d198a3d9d42e2703b
Justin Tadlock <justintadlock@gmail.com>
2018-04-19 12:55:18 -0500

    Update copyright dates.

--- inc/class-role.php
+++ inc/class-role.php
@@ -6,7 +6,7 @@
  * @package    Members
  * @subpackage Includes
  * @author     Justin Tadlock <justintadlock@gmail.com>
- * @copyright  Copyright (c) 2009 - 2017, Justin Tadlock
+ * @copyright  Copyright (c) 2009 - 2018, Justin Tadlock
  * @link       https://themehybrid.com/plugins/members
  * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
